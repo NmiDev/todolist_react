2feb35e4b96a5ff561919fe026a2a39976364964
NmiDev <missiaen.nicolas@gmail.com>
2019-03-13 21:02:25 +0100

    App is over without React component

--- src/index.js
+++ src/index.js
@@ -23,17 +23,21 @@ const tasks = [
 const app = {
   // Properties
   container: document.getElementById('app'), // App target
-  count: 0, // Count undo task
 
   // Init
   init: () => {
     // Application loaded
     console.log('Ready Player One');
 
+    // Reset container content
+    app.container.innerHTML = '';
+
     // Loading form
     app.loadForm();
-    // Loading task
-    app.loadTasks();
+    // Loading counter
+    app.loadCounter();
+    // Loading list
+    app.loadList();
   },
 
   // Method loadForm
@@ -52,26 +56,62 @@ const app = {
     app.container.appendChild(form);
   },
 
-  // Method loadTasks
-  loadTasks: () => {
-    // Title for the list
-    const title = document.createElement('h1');
-    const plural = app.count > 1 ? 's' : '';
-    title.setAttribute('id', 'title');
-    title.textContent = `${app.count} tâche${plural} en cours`;
-    app.container.appendChild(title);
+  // Method loadCounter
+  loadCounter: () => {
+    // Create counter
+    const counter = document.createElement('div');
+    counter.setAttribute('id', 'counter');
+    // Counter value
+    const undoneTasks = tasks.filter(task => !task.done);
+    const count = undoneTasks.length;
+    const plural = count > 1 ? 's' : '';
+    counter.textContent = `${count} tâche${plural} en cours`;
+    // Add counter to the container
+    app.container.appendChild(counter);
+  },
+
+  // Method loadList
+  loadList: () => {
     // List of tasks
     const list = document.createElement('ul');
     list.setAttribute('id', 'list');
-    // Item list with task
-    tasks.forEach((task) => {
-      const li = document.createElement('li');
-      li.setAttribute('class', 'task');
-      li.textContent = task.label;
-      list.appendChild(li);
-    });
     // Add list of tasks to the container
     app.container.appendChild(list);
+    // Item list with task
+    tasks.forEach(app.loadTask);
+  },
+
+  // Method loadTask
+  loadTask: (task) => {
+    // Target list
+    const list = document.getElementById('list');
+    // Create li
+    const li = document.createElement('li');
+    // Set class for task li
+    li.setAttribute('class', 'task');
+    if (task.done) {
+      li.classList.add('task--done');
+    }
+    // Task label
+    const label = document.createElement('span');
+    label.textContent = task.label;
+    // Task checbox
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.checked = task.done;
+    // Handler for check task
+    checkbox.addEventListener('change', () => {
+      // eslint-disable-next-line no-param-reassign
+      task.done = !task.done;
+      app.init();
+    });
+
+    // Add task items
+    li.appendChild(checkbox);
+    li.appendChild(label);
+
+    // Add task to the list
+    list.appendChild(li);
   },
 
   // Method addTask
